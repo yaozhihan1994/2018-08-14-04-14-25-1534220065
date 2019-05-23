8a3ada09409b5e4491f46462eec611c85692d3b7
yaozhihan1994 <yaozhihan1994@163.com>
2019-05-24 00:18:45 +0800

    finshed

--- main/main.js
+++ main/main.js
@@ -1,4 +1,19 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(x) {
+	var len1 = new String("._. ... ._. ._. ... ._. ._. ._. ._. ._.").split(" ");
+	var len2 = new String("|.| ..| ._| ._| |_| |_. |_. ..| |_| |_|").split(" ");
+	var len3 = new String("|_| ..| |_. ._| ..| ._| |_| ..| |_| ..|").split(" ");
+	var len = [len1, len2, len3];
+	var result = "";
+	var ret = x.split("");
+	for(var i = 0; i<3; i++){
+		var num = 0;
+		var tmp = "";
+		for(var j =0; j<x.length; j++){
+			num = ret[j].valueOf();
+			tmp+=len[i][num]+" ";
+		}
+		tmp= tmp.substring(0,x.length*4 -1)+"\n";
+		result+=tmp;
+	}
+	return result;
 };
